7e1e5827264dffcb4f874e02bbf24f987d58e7d0
Jan Marais <maraisjandre9@gmail.com>
2017-01-20 09:55:05 +0200

    make code more readable

--- cartpole_Qlearn_lm.R
+++ cartpole_Qlearn_lm.R
@@ -1,17 +1,17 @@
+# load necessary libraries
 library(gym)
 library(tidyverse)
 
-# adjust functions
-my_env_reset <- function (x, instance_id) {
+# adjust env_reset and env_step functions for custom output
+my_env_reset <- function(x, instance_id) {
   route <- paste0("/v1/envs/", instance_id, "/reset/", sep = "")
   response <- post_request(x, route)
   observation <- response[["observation"]]
   names(observation) <- paste0("obs", 1:length(observation))
   observation
-  #unlist(observation)
 }
 
-my_env_step <- function (x, instance_id, action, render = FALSE) {
+my_env_step <- function(x, instance_id, action, render = FALSE) {
   route <- paste0("/v1/envs/", instance_id, "/step/", sep = "")
   data <- list(instance_id = instance_id, action = action, 
                render = render)
@@ -25,40 +25,46 @@ my_env_step <- function (x, instance_id, action, render = FALSE) {
        info = info)
 }
 
+
+# Environment setup on R side
 remote_base <- "http://127.0.0.1:5000"  
 client <- create_GymClient(remote_base) 
 
 env_id <- "CartPole-v0"
 instance_id <- env_create(client, env_id)
 
-# function to determine action given state and model
+# function to determine action given state, model, action space and error
 my_agent <- function(obs, mod, acts = 0:1, epsi = eps) {
   if(!is.null(mod)) {
     temp <- obs[nrow(obs), ] %>% 
       bind_rows(obs[nrow(obs), ]) %>% 
       mutate(action = acts)
-    #temp <- as.matrix(temp)
     surv_prob <- predict(mod, temp)
     ought_to <- rank(surv_prob, ties.method = "random")[2] - 1
     ifelse(runif(1) > epsi, acts[acts %in% ought_to], acts[!(acts %in% ought_to)])
   } else sample(acts, 1)
-  
 }
 
-######################################
-# Algo
-######################################
-
-outdir <- "C:/Users/Jan/Documents/openAI/tmp/random-agent-results"
+# run to monitor algorithm
+outdir <- "~/lm-agent-results"
 env_monitor_start(client, instance_id, outdir, force = TRUE, resume = FALSE)
-gam_pam <- 0.9
-eps <- 0.1
 
+
+#### Start of algorithm ####
+
+gam_pam <- 0.9  # discounting parameter
+eps <- 0.1  # probability of not choosing the estimated best action (decrease over time?)
+
+# create and clear objects to be used in algo
 hist_data <- NULL
 mod <- NULL
 obs <- NULL
 score <- NULL
-for(j in 1:200) {
+mean_score <- 0
+cnt <- 0
+# start
+while(cnt < 100 | mean_score < 195) {
+  cnt <- cnt + 1
   action <- NULL
   obs <- as.data.frame(my_env_reset(client, instance_id))
   for(i in 1:200) {
@@ -68,10 +74,7 @@ for(j in 1:200) {
       obs <- bind_rows(obs, results$obs)
   }
   score <- c(score, nrow(obs))
-  # plot(score, type = "l",
-  #      main = paste(mean(score[length(score):(ifelse(length(score)>100, 
-  #                                                    length(score) - 100,
-  #                                                    1))])))
+  mean_score <- mean(tail(score, 100))
   
   episode_data <- obs %>% 
     mutate(action = action,
@@ -81,16 +84,13 @@ for(j in 1:200) {
   
   hist_data <- bind_rows(hist_data, episode_data)
   mod <- lm(q ~ .*., data = hist_data)
-  #mod <- glmnet(x = as.matrix(hist_data %>% select(-q)),
-  # y = hist_data$q,
-  # family = "gaussian")
-  #eps <-eps/(exp(0.005*(i-1)))
-  #if(score[length(score)] == max(score)) eps <- 0.999 * eps
-  #print(eps)
 }
+# end
+
+# stop monitoring
 env_monitor_close(client, instance_id)
 
-upload(
-  client,
-  training_dir = 'C:\\Users\\Jan\\Documents\\openAI\\tmp\\random-agent-results',
-  api_key = "sk_mtuzQZRZS1eFaLkGpGZcbg")
+# upload to openAI
+upload(client,
+       training_dir = outdir,
+       api_key = "sk_mtuzQZRZS1eFaLkGpGZcbg")
